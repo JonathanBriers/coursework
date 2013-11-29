ac20891877f02763f132e7971fe9824b887fbdf9
Jonathan Briers <jonathan.briers@btinternet.com>
2013-11-29 17:02:12 +0000

    Add a static pages controller

---
+++ coursework/app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def join
+  end
+
+  def needs
+  end
+end
---
+++ coursework/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coursework::Application.config.secret_key_base = '1cd4d076300ade2a56dd09078d115fc3832702fbb8648c81aaa1e8d1687273adaee7d6969f68bb35992351a2703c9bd4d88e4d74101d5c2c61e0c27d89f65501'
