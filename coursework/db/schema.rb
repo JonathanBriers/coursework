0aa3357e96c1121b4fd0f56fdfe0afe9c728397f
Jonathan Briers <jonathan.briers@btinternet.com>
2013-12-01 17:40:18 +0000

    Ran rake db:migrate

--- coursework/db/schema.rb
+++ coursework/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130222005) do
+ActiveRecord::Schema.define(version: 20131201173125) do
 
   create_table "admin_users", force: true do |t|
     t.string   "username"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20131130222005) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "username"
+  end
+
+  create_table "super_users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
